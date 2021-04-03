06dcdf96f27260a1f808261d541ae10738449d01
Brhaka <martinez@brhaka.com>
2021-04-03 19:03:15 +0100

    USE_COLORS compile 'flag'

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
-SRCS = ./image_to_ascii.c
-NAME = ImageToASCII
+SRCS := ./image_to_ascii.c
+NAME := ImageToASCII
+USE_COLORS = 1
 
 all:	UNIX WIN
 
 $(NAME):	UNIX WIN
 
 UNIX:
-	gcc $(SRCS) -static -lm -o $(NAME)
+	gcc $(SRCS) -static -lm -D USE_COLORS=$(USE_COLORS) -o $(NAME)
 
 WIN:
-	i686-w64-mingw32-g++ $(SRCS) --static -o $(NAME)
+	i686-w64-mingw32-g++ $(SRCS) --static -D USE_COLORS=$(USE_COLORS) -o $(NAME)
 
 clean:
 	rm -f $(NAME)
@@ -17,4 +18,4 @@ clean:
 
 re:		clean $(NAME)
 
-.PHONY:	re clean UNIX WIN all
+.PHONY:	re clean WIN UNIX all
--- colors.h
+++ colors.h
@@ -8,14 +8,34 @@
 #ifndef COLORS_H
 # define COLORS_H
 
-# define COLOR_RED		"\x1B[31m"
-# define COLOR_GREEN	"\x1B[32m"
-# define COLOR_YELLOW	"\x1B[33m"
-# define COLOR_BLUE		"\x1B[34m"
-# define COLOR_MAGENTA	"\x1B[35m"
-# define COLOR_CYAN		"\x1B[36m"
-# define COLOR_WHITE	"\x1B[37m"
-# define COLOR_BLACK	"\x1B[30m"
-# define COLOR_RESET	"\x1B[0m"
+# ifndef USE_COLORS
+#  define USE_COLORS 1
+# endif
+
+# if USE_COLORS == 1
+
+#  define COLOR_RED		"\x1B[31m"
+#  define COLOR_GREEN	"\x1B[32m"
+#  define COLOR_YELLOW	"\x1B[33m"
+#  define COLOR_BLUE	"\x1B[34m"
+#  define COLOR_MAGENTA	"\x1B[35m"
+#  define COLOR_CYAN	"\x1B[36m"
+#  define COLOR_WHITE	"\x1B[37m"
+#  define COLOR_BLACK	"\x1B[30m"
+#  define COLOR_RESET	"\x1B[0m"
+
+# else
+
+#  define COLOR_RED		""
+#  define COLOR_GREEN	""
+#  define COLOR_YELLOW	""
+#  define COLOR_BLUE	""
+#  define COLOR_MAGENTA	""
+#  define COLOR_CYAN	""
+#  define COLOR_WHITE	""
+#  define COLOR_BLACK	""
+#  define COLOR_RESET	""
+
+# endif
 
 #endif
